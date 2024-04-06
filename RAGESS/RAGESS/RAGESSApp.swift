ec4a70b434f452cd3902dee629574c27355db738
OnagaHaruki <haloupgarou@gmail.com>
2024-04-06 18:51:26 +0900

    プロジェクトを作成

---
+++ RAGESS/RAGESS/RAGESSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAGESSApp.swift
+//  RAGESS
+//
+//  Created by ockey12 on 2024/04/06.
+//
+
+import SwiftUI
+
+@main
+struct RAGESSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

e03d14d7791a7e729dd910d847c3ab5bad058cac
OnagaHaruki <haloupgarou@gmail.com>
2024-04-18 01:15:34 +0900

    Added a test method that attempts to insert a string at a negative line index

--- RAGESS/RAGESS/Tests/TypeAnnotationClientTests/String+InsertTests.swift
+++ RAGESS/RAGESS/Tests/TypeAnnotationClientTests/String+InsertTests.swift
@@ -28,4 +28,23 @@ final class String_InsertTests: XCTestCase {
         target.insert(additionalString, into: position)
         XCTAssertEqual(target, "This is a sentence.")
     }
+
+    func test_insertAtNegativeLineNumber() {
+        var target = """
+        This sentence is line 0.
+        This sentence is line 1.
+        This sentence is line 2.
+        """
+        let additionalString = "This sentence is negative line."
+        let position = Position(line: -1, utf16index: 0)
+        target.insert(additionalString, into: position)
+        XCTAssertEqual(
+            target,
+            """
+            This sentence is line 0.
+            This sentence is line 1.
+            This sentence is line 2.
+            """
+        )
+    }
 }

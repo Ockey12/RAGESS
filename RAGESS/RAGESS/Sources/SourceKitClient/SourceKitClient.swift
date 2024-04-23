6ce1ad7d8cb1e4d4a46c53c8eb5d2828188dccda
OnagaHaruki <haloupgarou@gmail.com>
2024-04-23 15:26:20 +0900

    added `sendCursorInfoRequest`

--- RAGESS/RAGESS/Sources/SourceKitClient/SourceKitClient.swift
+++ RAGESS/RAGESS/Sources/SourceKitClient/SourceKitClient.swift
@@ -5,4 +5,28 @@
 //  Created by ockey12 on 2024/04/23.
 //
 
-import Foundation
+import Dependencies
+import DependenciesMacros
+import SourceKittenFramework
+
+@DependencyClient
+public struct SourceKitClient {
+    public var sendCursorInfoRequest: @Sendable(
+        _ file: String,
+        _ offset: Int,
+        _ arguments: [String]
+    ) async throws -> Void
+}
+
+extension SourceKitClient: DependencyKey {
+    public static let liveValue: Self = {
+        return .init(
+            sendCursorInfoRequest: { file, offset, arguments in
+                let byteCount = ByteCount(offset)
+                let request = Request.cursorInfo(file: file, offset: byteCount, arguments: arguments)
+                let response = try await request.asyncSend()
+                dump(response)
+            }
+        )
+    }()
+}

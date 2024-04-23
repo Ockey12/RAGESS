83d7c168117143feea3cbe30db98698afe173402
OnagaHaruki <haloupgarou@gmail.com>
2024-04-23 18:49:13 +0900

    added test method

--- RAGESS/RAGESS/Tests/SourceKitClientTests/String+ByteOffsetTests.swift
+++ RAGESS/RAGESS/Tests/SourceKitClientTests/String+ByteOffsetTests.swift
@@ -5,32 +5,45 @@
 //  Created by ockey12 on 2024/04/23.
 //
 
+import LanguageServerProtocol
 import XCTest
 
 @testable import SourceKitClient
 
 final class ByteOffsetTests: XCTestCase {
-    let target = """
-    private struct Affected {
-        let affected: Affecting = Affecting()
-        let propertyCaller: Int = Affecting().property
-        let methodCaller: Int = Affecting().getValue()
-
-        func get() {
-            let value: Int = affected.getValue()
-        }
-    }
-
-    """
-
     func test_endOfString() {
+        let target = """
+        private struct Affected {
+            let affected: Affecting = Affecting()
+            let propertyCaller: Int = Affecting().property
+            let methodCaller: Int = Affecting().getValue()
+
+            func get() {
+                let value: Int = affected.getValue()
+            }
+        }
+
+        """
+
         do {
             let endOfStringPosition = target.lastPosition
-            print(endOfStringPosition)
             let offset = try target.getByteOffset(position: endOfStringPosition)
             XCTAssertEqual(offset, 241)
         } catch let error as String.ByteOffsetError {
             XCTFail("Error: \(error.localizedDescription)")
+        } catch {
+            XCTFail("Unexpected error: \(error)")
+        }
+    }
+
+    func test_invalidNumberOfLines() {
+        let target = "Swift"
+        do {
+            let invalidPosition = Position(line: 1, utf16index: 0)
+            let offset = try target.getByteOffset(position: invalidPosition)
+            XCTFail("Expected ByteOffsetError.invalidNumberOfLines")
+        } catch let String.ByteOffsetError.invalidNumberOfLines(line) {
+            XCTAssertEqual(line, 1)
         } catch {
             XCTFail("Unexpected error: \(error)")
         }

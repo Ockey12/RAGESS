0385070eae0142cd16e9aa1691943efd57395af5
OnagaHaruki <haloupgarou@gmail.com>
2024-04-28 18:12:31 +0900

    implemented Directory structure

--- RAGESS/RAGESS/Sources/XcodeObject/Directory.swift
+++ RAGESS/RAGESS/Sources/XcodeObject/Directory.swift
@@ -1,8 +1,24 @@
 //
-//  File.swift
-//  
+//  Directory.swift
 //
-//  Created by オナガ・ハルキ on 2024/04/28.
+//
+//  Created by ockey12 on 2024/04/28.
 //
 
 import Foundation
+
+public struct Directory: Identifiable {
+    public var id: String {
+        path
+    }
+
+    public let path: String
+    public var subDirectories: [Self]
+    public var files: [SourceFile]
+
+    public init(path: String, subDirectories: [Self], files: [SourceFile]) {
+        self.path = path
+        self.subDirectories = subDirectories
+        self.files = files
+    }
+}

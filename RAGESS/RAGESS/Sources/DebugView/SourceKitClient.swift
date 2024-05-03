4c4528078df709e673e94f8a32d4c4d446968406
Ockey12 <haloupgarou@gmail.com>
2024-05-04 03:27:25 +0900

    removed unnecessary sections for debugging

--- RAGESS/RAGESS/Sources/DebugView/SourceKitClient.swift
+++ RAGESS/RAGESS/Sources/DebugView/SourceKitClient.swift
@@ -26,124 +26,6 @@ public struct SourceKitClientDebugger {
         var allFilePathsInProject: [String] = []
         var buildSettings: [String: String] = [:]
         var packages: [PackageObject] = []
-        var compilerArguments: [String] {
-            [
-                "-vfsoverlay",
-                "/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Intermediates.noindex/index-overlay.yaml",
-                "-module-name",
-                "DebugView",
-                "-Onone",
-                "-enforce-exclusivity=checked"
-            ]
-                + allFilePathsInProject
-                + [
-                    "-DSWIFT_PACKAGE",
-                    "-DDEBUG",
-                    "-Xcc",
-                    "-fmodule-map-file=/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Intermediates.noindex/GeneratedModuleMaps/SourceKit.modulemap",
-                    "-Xcc",
-                    "-fmodule-map-file=/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Intermediates.noindex/GeneratedModuleMaps/Clang_C.modulemap",
-                    "-DXcode",
-                    "-sdk",
-                    "/Applications/Xcode-15.2.0.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk",
-                    "-target",
-                    "arm64-apple-macos14.0",
-                    "-g",
-                    "-module-cache-path",
-                    "/Users/onaga/Library/Developer/Xcode/DerivedData/ModuleCache.noindex",
-                    "-Xfrontend",
-                    "-serialize-debugging-options",
-                    "-enable-testing",
-                    "-swift-version",
-                    "5",
-                    "-I",
-                    "/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Products/Debug",
-                    "-I",
-                    "/Applications/Xcode-15.2.0.app/Contents/Developer/Platforms/MacOSX.platform/Developer/usr/lib",
-                    "-F",
-                    "/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Products/Debug/PackageFrameworks",
-                    "-F",
-                    "/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Products/Debug",
-                    "-F",
-                    "/Applications/Xcode-15.2.0.app/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks",
-                    "-Xfrontend",
-                    "-load-plugin-executable",
-                    "-Xfrontend",
-                    "/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Products/Debug/CasePathsMacros#CasePathsMacros",
-                    "-Xfrontend",
-                    "-load-plugin-executable",
-                    "-Xfrontend",
-                    "/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Products/Debug/ComposableArchitectureMacros#ComposableArchitectureMacros",
-                    "-Xfrontend",
-                    "-load-plugin-executable",
-                    "-Xfrontend",
-                    "/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Products/Debug/DependenciesMacrosPlugin#DependenciesMacrosPlugin",
-                    "-Xfrontend",
-                    "-load-plugin-executable",
-                    "-Xfrontend",
-                    "/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Products/Debug/PerceptionMacros#PerceptionMacros",
-                    "-Xfrontend",
-                    "-experimental-allow-module-with-compiler-errors",
-                    "-Xfrontend",
-                    "-empty-abi-descriptor",
-                    "-Xcc",
-                    "-fretain-comments-from-system-headers",
-                    "-Xcc",
-                    "-Xclang",
-                    "-Xcc",
-                    "-detailed-preprocessing-record",
-                    "-Xcc",
-                    "-Xclang",
-                    "-Xcc",
-                    "-fmodule-format=raw",
-                    "-Xcc",
-                    "-ferror-limit=10",
-                    "-Xcc",
-                    "-Xclang",
-                    "-Xcc",
-                    "-fallow-pch-with-compiler-errors",
-                    "-Xcc",
-                    "-Xclang",
-                    "-Xcc",
-                    "-fallow-pcm-with-compiler-errors",
-                    "-Xcc",
-                    "-Wno-non-modular-include-in-framework-module",
-                    "-Xcc",
-                    "-Wno-incomplete-umbrella",
-                    "-Xcc",
-                    "-fmodules-validate-system-headers",
-                    "-Xfrontend",
-                    "-package-name",
-                    "-Xfrontend",
-                    "ragess",
-                    "-Xcc",
-                    "-I/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Intermediates.noindex/RAGESS.build/Debug/DebugView.build/swift-overrides.hmap",
-                    "-Xcc",
-                    "-I/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/SourcePackages/checkouts/swift-system/Sources/CSystem/include",
-                    "-Xcc",
-                    "-I/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/SourcePackages/checkouts/swift-tools-support-core/Sources/TSCclibc/include",
-                    "-Xcc",
-                    "-I/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/SourcePackages/checkouts/Yams/Sources/CYaml/include",
-                    "-Xcc",
-                    "-I/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/SourcePackages/checkouts/SourceKitten/Source/SourceKit/include",
-                    "-Xcc",
-                    "-I/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/SourcePackages/checkouts/SourceKitten/Source/Clang_C/include",
-                    "-Xcc",
-                    "-I/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Products/Debug/include",
-                    "-Xcc",
-                    "-I/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Intermediates.noindex/RAGESS.build/Debug/DebugView.build/DerivedSources-normal/arm64",
-                    "-Xcc",
-                    "-I/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Intermediates.noindex/RAGESS.build/Debug/DebugView.build/DerivedSources/arm64",
-                    "-Xcc",
-                    "-I/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/Build/Intermediates.noindex/RAGESS.build/Debug/DebugView.build/DerivedSources",
-                    "-Xcc",
-                    "-DSWIFT_PACKAGE",
-                    "-Xcc",
-                    "-DDEBUG=1",
-                    "-working-directory",
-                    "/Users/onaga/RAGESS/RAGESS/RAGESS"
-                ]
-        }
 
         public init(filePath: String) {
             self.filePath = filePath
@@ -158,7 +40,6 @@ public struct SourceKitClientDebugger {
         case cursorInfoTapped
         case compilerArgumentsResponse(Result<[String], Error>)
         case cursorInfoResponse(Result<[String: SourceKitRepresentable], Error>)
-        case compilerArgumentsGeneratorTapped
         case binding(BindingAction<State>)
     }
 
@@ -202,8 +83,6 @@ public struct SourceKitClientDebugger {
 
             case .cursorInfoTapped:
                 state.cursorInfoResponse = CursorInfoResponse()
-                print("\nSourceKitClientDebugger")
-                dump(state.packages)
                 let generator = CompilerArgumentsGenerator(
                     targetFilePath: state.filePath,
                     buildSettings: state.buildSettings,
@@ -217,17 +96,7 @@ public struct SourceKitClientDebugger {
                 }
 
             case let .compilerArgumentsResponse(.success(arguments)):
-                print("Compiler Arguments")
-                for argument in arguments {
-                    print(argument)
-                }
-                print("")
-
-                let arg = state.compilerArguments
-                print("arguments == arg: \(arguments == arg)")
-
                 return .run { [filePath = state.filePath, offset = state.offset, allFilePathsInProject = state.allFilePathsInProject] send in
-                    print("\n=====send cursorinfo request=====\n")
                     await send(.cursorInfoResponse(Result {
                         try await sourceKitClient.sendCursorInfoRequest(
                             file: filePath,
@@ -283,36 +152,11 @@ public struct SourceKitClientDebugger {
                         print("Unexpected Response")
                         dump(response)
                     }
-//                    } else {
-//                        fatalError("Unexpected Response [\(key): \(value)]")
-//                    }
                 }
                 return .none
 
             case let .cursorInfoResponse(.failure(error)):
                 print(error)
-                return .none
-
-            case .compilerArgumentsGeneratorTapped:
-//                let generator = CompilerArgumentsGenerator(
-//                    targetFilePath: state.filePath,
-//                    buildSettings: state.buildSettings,
-//                    xcodeprojPath: "",
-//                    moduleName: "",
-//                    sourceFilePaths: []
-//                )
-//                print("Start CompilerArgumentsGenerator.getIncludePaths")
-//                let includePaths = generator.getIncludePaths(
-//                    in: "/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/SourcePackages/checkouts",
-//                    ignoredDirectories: ["swift-package-manager"]
-//                )
-//                for path in includePaths {
-//                    print(path)
-//                }
-//                print("End CompilerArgumentsGenerator.getIncludePaths")
-                for argument in state.compilerArguments {
-                    print(argument)
-                }
                 return .none
 
             case .binding:
@@ -337,9 +181,6 @@ public struct SourceKitClientDebugView: View {
                 TextField("Symbol Name", text: $store.symbolName)
                 Button("Dump Symbol") {
                     store.send(.dumpSymbolTapped)
-                }
-                Button("CompilerArgumentsGenerator.resume()") {
-                    store.send(.compilerArgumentsGeneratorTapped)
                 }
                 Section {
                     TextEditor(text: $store.countedString)

5e5d3169d855a0fe17ecbd4183b8f9242c967eba
Ockey12 <haloupgarou@gmail.com>
2024-05-06 02:01:07 +0900

    added Function

---
+++ RAGESS/RAGESS/Sources/DeclarationType/Function.swift
@@ -0,0 +1,16 @@
+//
+//  Function.swift
+//
+//
+//  Created by ockey12 on 2024/05/06.
+//
+
+public struct Function {
+    public let name: String
+    public let accessLevel: AccessLevel
+
+    public init(name: String, accessLevel: AccessLevel) {
+        self.name = name
+        self.accessLevel = accessLevel
+    }
+}

3875f19e43ab5b0eeb4054dc923d74637120dae8
Ockey12 <haloupgarou@gmail.com>
2024-05-08 01:16:02 +0900

    renamed Function to FunctionObject

---
+++ RAGESS/RAGESS/Sources/TypeDeclaration/FunctionObject.swift
@@ -0,0 +1,27 @@
+//
+//  Function.swift
+//
+//
+//  Created by ockey12 on 2024/05/06.
+//
+
+import LanguageServerProtocol
+
+public struct FunctionObject {
+    public let name: String
+    public var fullPath: String
+    public let sourceCode: String
+    public let sourceRange: ClosedRange<Position>
+
+    public init(
+        name: String,
+        fullPath: String,
+        sourceCode: String,
+        sourceRange: ClosedRange<Position>
+    ) {
+        self.name = name
+        self.fullPath = fullPath
+        self.sourceCode = sourceCode
+        self.sourceRange = sourceRange
+    }
+}

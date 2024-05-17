592f3bf84ee64037e5ebd6018fc3cb367c84b965
Ockey12 <haloupgarou@gmail.com>
2024-05-17 18:50:32 +0900

    added `getAnnotatedDeclaration`

--- RAGESS/RAGESS/Sources/DeclarationExtractor/DeclarationExtractor.swift
+++ RAGESS/RAGESS/Sources/DeclarationExtractor/DeclarationExtractor.swift
@@ -5,6 +5,8 @@
 //  Created by ockey12 on 2024/05/05.
 //
 
+import SourceKitClient
+import Dependencies
 import SwiftParser
 import SwiftSyntax
 import TypeDeclaration
@@ -43,5 +45,169 @@ public struct DeclarationExtractor {
         result.append(contentsOf: visitor.getFunctionDeclarations())
 
         return result
+    }
+
+    private func getAnnotatedDeclaration<T: TypeDeclaration>(
+        _ typeObject: T,
+        buildSettings: [String: String],
+        sourceFilePaths: [String],
+        packages: [PackageObject]
+    ) async -> T {
+        @Dependency(SourceKitClient.self) var sourceKitClient
+        let argumentsGenerator = CompilerArgumentsGenerator(
+            targetFilePath: typeObject.fullPath,
+            buildSettings: buildSettings,
+            sourceFilePaths: sourceFilePaths,
+            packages: packages
+        )
+        guard let arguments = try? argumentsGenerator.generateArguments() else {
+            print("ERROR: \(#file) - \(#function): Cannot generate compiler arguments about \(typeObject.name).")
+            return typeObject
+        }
+
+        do {
+            let response = try await sourceKitClient.sendCursorInfoRequest(
+                file: typeObject.fullPath,
+                offset: typeObject.nameOffset,
+                sourceFilePaths: sourceFilePaths,
+                arguments: arguments
+            )
+
+            guard let annotatedDecl = response[CursorInfoResponseKeys.fullyAnnotatedDecl.key] as? String else {
+                print("ERROR: \(#file) - \(#function): Cannot find `key.fully_annotated_decl` about \(typeObject.name).")
+                return typeObject
+            }
+
+            var resultObject = typeObject
+            resultObject.annotatedDecl = annotatedDecl
+
+            for (index, variable) in typeObject.variables.enumerated() {
+                let annotatedVariable = await getAnnotatedDeclaration(
+                    variable,
+                    buildSettings: buildSettings,
+                    sourceFilePaths: sourceFilePaths,
+                    packages: packages
+                )
+                resultObject.variables[index] = annotatedVariable
+            }
+
+            for (index, function) in typeObject.functions.enumerated() {
+                let annotatedFunction = await getAnnotatedDeclaration(
+                    function,
+                    buildSettings: buildSettings,
+                    sourceFilePaths: sourceFilePaths,
+                    packages: packages
+                )
+                resultObject.functions[index] = annotatedFunction
+            }
+
+            for (index, nestingStruct) in resultObject.nestingStructs.enumerated() {
+                let annotatedStruct = await getAnnotatedDeclaration(
+                    nestingStruct,
+                    buildSettings: buildSettings,
+                    sourceFilePaths: sourceFilePaths,
+                    packages: packages
+                )
+                resultObject.nestingStructs[index] = annotatedStruct
+            }
+
+            for (index, nestingClass) in resultObject.nestingClasses.enumerated() {
+                let annotatedClass = await getAnnotatedDeclaration(
+                    nestingClass,
+                    buildSettings: buildSettings,
+                    sourceFilePaths: sourceFilePaths,
+                    packages: packages
+                )
+                resultObject.nestingClasses[index] = annotatedClass
+            }
+
+            for (index, nestingEnum) in resultObject.nestingEnums.enumerated() {
+                let annotatedEnum = await getAnnotatedDeclaration(
+                    nestingEnum,
+                    buildSettings: buildSettings,
+                    sourceFilePaths: sourceFilePaths,
+                    packages: packages
+                )
+            }
+
+            return resultObject
+        } catch {
+            return typeObject
+        }
+    }
+
+    private func getAnnotatedDeclaration<T>(
+        _ inputObject: T,
+        buildSettings: [String: String],
+        sourceFilePaths: [String],
+        packages: [PackageObject]
+    ) async -> T where T: VariableOwner, T: FunctionOwner {
+        @Dependency(SourceKitClient.self) var sourceKitClient
+        let argumentsGenerator = CompilerArgumentsGenerator(
+            targetFilePath: inputObject.fullPath,
+            buildSettings: buildSettings,
+            sourceFilePaths: sourceFilePaths,
+            packages: packages
+        )
+        guard let arguments = try? argumentsGenerator.generateArguments() else {
+            print("ERROR: \(#file) - \(#function): Cannot generate compiler arguments about \(inputObject.name).")
+            return inputObject
+        }
+
+        do {
+            let response = try await sourceKitClient.sendCursorInfoRequest(
+                file: inputObject.fullPath,
+                offset: inputObject.nameOffset,
+                sourceFilePaths: sourceFilePaths,
+                arguments: arguments
+            )
+
+            guard let annotatedDecl = response[CursorInfoResponseKeys.fullyAnnotatedDecl.key] as? String else {
+                print("ERROR: \(#file) - \(#function): Cannot find `key.fully_annotated_decl` about \(inputObject.name).")
+                return inputObject
+            }
+
+            var resultObject = inputObject
+            resultObject.annotatedDecl = annotatedDecl
+
+            for (index, variable) in inputObject.variables.enumerated() {
+                let annotatedVariable = await getAnnotatedDeclaration(
+                    variable,
+                    buildSettings: buildSettings,
+                    sourceFilePaths: sourceFilePaths,
+                    packages: packages
+                )
+                resultObject.variables[index] = annotatedVariable
+            }
+
+            for (index, function) in inputObject.functions.enumerated() {
+                let annotatedFunction = await getAnnotatedDeclaration(
+                    function,
+                    buildSettings: buildSettings,
+                    sourceFilePaths: sourceFilePaths,
+                    packages: packages
+                )
+                resultObject.functions[index] = annotatedFunction
+            }
+
+            return resultObject
+        } catch {
+            return inputObject
+        }
+    }
+}
+
+private extension String {
+    var annotatedDecl: String {
+        var decl = self
+        while let startIndex = decl.firstIndex(of: "<"),
+              let endIndex = decl[startIndex...].firstIndex(of: ">") {
+            decl.removeSubrange(startIndex...endIndex)
+        }
+
+        decl = decl.replacingOccurrences(of: "&gt;", with: ">")
+        decl = decl.replacingOccurrences(of: "&lt;", with: "<")
+
+        return decl
     }
 }

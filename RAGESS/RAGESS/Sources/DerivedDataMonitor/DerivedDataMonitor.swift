748976773ac57c91d25a4d950ad9aa8a5b4d9cb1
Ockey12 <haloupgarou@gmail.com>
2024-05-20 16:45:53 +0900

    added `DerivedDataMonitor`

--- RAGESS/RAGESS/Sources/DerivedDataMonitor/DerivedDataMonitor.swift
+++ RAGESS/RAGESS/Sources/DerivedDataMonitor/DerivedDataMonitor.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+import Cocoa
+
+public class DerivedDataMonitor {
+    let directoryPath: String
+    private var watchTask: Task<Void, Never>?
+    private var changeStream: AsyncStream<Void>?
+
+    public init(directoryPath: String) {
+        self.directoryPath = directoryPath
+    }
+
+    public func startMonitoring() -> AsyncStream<Void> {
+        let stream = AsyncStream<Void> { continuation in
+            watchTask = Task {
+                let fileDescriptor = open(directoryPath, O_EVTONLY)
+                guard fileDescriptor != -1 else {
+                    print("Failed to open directory: \(directoryPath)")
+                    continuation.finish()
+                    return
+                }
+
+                let source = DispatchSource.makeFileSystemObjectSource(
+                    fileDescriptor: fileDescriptor,
+                    eventMask: .write,
+                    queue: .global()
+                )
+
+                source.setEventHandler {
+                    print("Directory content changed.")
+                    continuation.yield()
+                }
+
+                source.setCancelHandler {
+                    close(fileDescriptor)
+                    continuation.finish()
+                }
+
+                source.resume()
+            }
+        }
+
+        changeStream = stream
+        return stream
+    }
+
+    public func stopMonitoring() {
+        watchTask?.cancel()
+        watchTask = nil
+        changeStream = nil
+    }
+}

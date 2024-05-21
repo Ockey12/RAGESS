8258a308b8f74c6972129bc87c7bfccae9b4ba11
Ockey12 <haloupgarou@gmail.com>
2024-05-22 03:07:58 +0900

    added String+systemSize50Width

---
+++ RAGESS/RAGESS/Sources/SwiftDiagramView/String+systemSize50Width.swift
@@ -0,0 +1,19 @@
+//
+//  String+systemSize50Width.swift
+//
+//  
+//  Created by Ockey12 on 2024/05/22
+//  
+//
+
+import Foundation
+
+extension String {
+    var systemSize50Width: CGFloat {
+        var width: CGFloat = 0
+        for character in self {
+            width += character.systemSize50Width
+        }
+        return width
+    }
+}

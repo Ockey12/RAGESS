151c53110da3d25582418e657cb67fd70dd0b98e
Ockey12 <haloupgarou@gmail.com>
2024-05-23 05:44:56 +0900

    added `ExtractSuperClassObjects`

---
+++ RAGESS/RAGESS/Sources/SwiftDiagramView/TypeView/Class/ExtractSuperClassObjects.swift
@@ -0,0 +1,22 @@
+//
+//  ExtractSuperClassObjects.swift
+//
+//  
+//  Created by Ockey12 on 2024/05/23
+//  
+//
+
+import TypeDeclaration
+
+func extractSuperClassObjects(
+    by subClassObject: any Inheritable,
+    allDeclarationObjects: [any DeclarationObject]
+) -> ClassObject? {
+    let classInheritanceDependency = subClassObject.objectsThatAreCalledByThisObject.first(where: { $0.kind == .classInheritance })
+
+    guard let classInheritanceDependency else {
+        return nil
+    }
+    return allDeclarationObjects.first(where: { $0.id == classInheritanceDependency.definitionObject.id }) as? ClassObject
+
+}

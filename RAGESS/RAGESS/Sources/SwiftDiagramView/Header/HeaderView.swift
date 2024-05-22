ae53939216c38123d1bf0f28cc6158e206f0e23b
Ockey12 <haloupgarou@gmail.com>
2024-05-22 14:42:15 +0900

    applied `HeaderReducer` to `HeaderView`

--- RAGESS/RAGESS/Sources/SwiftDiagramView/Header/HeaderView.swift
+++ RAGESS/RAGESS/Sources/SwiftDiagramView/Header/HeaderView.swift
@@ -6,95 +6,128 @@
 //
 //
 
+import ComposableArchitecture
 import SwiftUI
 import TypeDeclaration
 
 struct HeaderView: View {
-    let object: any HasHeader
-    let bodyWidth: CGFloat
+    let store: StoreOf<HeaderReducer>
 
     let arrowTerminalWidth = ComponentSizeValues.arrowTerminalWidth
     let itemHeight = ComponentSizeValues.itemHeight
 
     var body: some View {
         ZStack(alignment: .topLeading) {
-            IndexView(object: object)
+            IndexView(object: store.object)
                 .offset(x: arrowTerminalWidth, y: 0)
 
-            HeaderViewWithoutIndex(object: object, bodyWidth: bodyWidth)
+            HeaderViewWithoutIndex(object: store.object, bodyWidth: store.bodyWidth)
                 .offset(x: 0, y: itemHeight)
         } // ZStack
+        .frame(
+            width: store.bodyWidth + ComponentSizeValues.arrowTerminalWidth*2,
+            height: 300
+        )
+        .onTapGesture {
+            store.send(.clicked)
+        }
     }
 }
 
 #Preview {
-    Group {
+
+    let protocolObject = ProtocolObject(
+        name: "SampleProtocol",
+        nameOffset: 0,
+        fullPath: "",
+        sourceCode: "",
+        positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+        offsetRange: 0 ... 1
+    )
+
+    let structObject = StructObject(
+        name: "SampleStruct",
+        nameOffset: 0,
+        fullPath: "",
+        sourceCode: "",
+        positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+        offsetRange: 0 ... 1
+    )
+
+    let classObject = ClassObject(
+        name: "SampleClass",
+        nameOffset: 0,
+        fullPath: "",
+        sourceCode: "",
+        positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+        offsetRange: 0 ... 1
+    )
+
+    let enumObject = EnumObject(
+        name: "SampleEnum",
+        nameOffset: 0,
+        fullPath: "",
+        sourceCode: "",
+        positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+        offsetRange: 0 ... 1
+    )
+
+    return Group {
         HeaderView(
-            object: ProtocolObject(
-                name: "SampleProtocol",
-                nameOffset: 0,
-                fullPath: "",
-                sourceCode: "",
-                positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
-                offsetRange: 0 ... 1
-            ),
-            bodyWidth: max(
-                "SampleProtocol".systemSize50Width,
-                ComponentSizeValues.bodyMinWidth
+            store: .init(
+                initialState: HeaderReducer.State(
+                    object: protocolObject,
+                    bodyWidth: max(
+                        protocolObject.name.systemSize50Width,
+                        ComponentSizeValues.bodyMinWidth
+                    )
+                ),
+                reducer: { HeaderReducer() }
             )
         )
-        .frame(width: 1000, height: 300)
-        .padding()
+        .border(.pink)
 
         HeaderView(
-            object: StructObject(
-                name: "SampleStruct",
-                nameOffset: 0,
-                fullPath: "",
-                sourceCode: "",
-                positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
-                offsetRange: 0 ... 1
-            ),
-            bodyWidth: max(
-                "SampleStruct".systemSize50Width,
-                ComponentSizeValues.bodyMinWidth
+            store: .init(
+                initialState: HeaderReducer.State(
+                    object: structObject,
+                    bodyWidth: max(
+                        structObject.name.systemSize50Width,
+                        ComponentSizeValues.bodyMinWidth
+                    )
+                ),
+                reducer: { HeaderReducer() }
             )
         )
-        .frame(width: 1000, height: 300)
-        .padding()
+        .border(.pink)
 
         HeaderView(
-            object: ClassObject(
-                name: "SampleClass",
-                nameOffset: 0,
-                fullPath: "",
-                sourceCode: "",
-                positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
-                offsetRange: 0 ... 1
-            ),
-            bodyWidth: max(
-                "SampleClass".systemSize50Width,
-                ComponentSizeValues.bodyMinWidth
+            store: .init(
+                initialState: HeaderReducer.State(
+                    object: classObject,
+                    bodyWidth: max(
+                        classObject.name.systemSize50Width,
+                        ComponentSizeValues.bodyMinWidth
+                    )
+                ),
+                reducer: { HeaderReducer() }
             )
         )
-        .frame(width: 1000, height: 300)
-        .padding()
+        .border(.pink)
 
         HeaderView(
-            object: EnumObject(
-                name: "SampleEnum",
-                nameOffset: 0,
-                fullPath: "",
-                sourceCode: "",
-                positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
-                offsetRange: 0 ... 1
-            ),
-            bodyWidth: max(
-                "SampleEnum".systemSize50Width,
-                ComponentSizeValues.bodyMinWidth
+            store: .init(
+                initialState: HeaderReducer.State(
+                    object: enumObject,
+                    bodyWidth: max(
+                        enumObject.name.systemSize50Width,
+                        ComponentSizeValues.bodyMinWidth
+                    )
+                ),
+                reducer: { HeaderReducer() }
             )
         )
-        .frame(width: 1000, height: 300)
-        .padding()
+        .border(.pink)
     }
+    .frame(width: 1000, height: 400)
 }

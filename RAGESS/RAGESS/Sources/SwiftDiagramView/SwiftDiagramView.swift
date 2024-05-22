3339697bef3be5da02a464ec01cf56edbe28c819
Ockey12 <haloupgarou@gmail.com>
2024-05-23 05:19:22 +0900

    added `SwiftDiagramView`

---
+++ RAGESS/RAGESS/Sources/SwiftDiagramView/SwiftDiagramView.swift
@@ -0,0 +1,29 @@
+//
+//  SwiftDiagramView.swift
+//
+//  
+//  Created by Ockey12 on 2024/05/23
+//  
+//
+
+import ComposableArchitecture
+import SwiftUI
+import TypeDeclaration
+
+public struct SwiftDiagramView: View {
+    let store: StoreOf<SwiftDiagramReducer>
+
+    public init(store: StoreOf<SwiftDiagramReducer>) {
+        self.store = store
+    }
+
+    public var body: some View {
+        ScrollView(.horizontal) {
+            HStack(alignment: .top, spacing: 100) {
+                ForEach(store.scope(state: \.structs, action: \.structs)) { structStore in
+                    StructView(store: structStore)
+                }
+            }
+        }
+    }
+}

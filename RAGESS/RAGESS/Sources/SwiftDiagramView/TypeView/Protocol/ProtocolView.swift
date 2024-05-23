f445d75a007e95cd746d3a07391290dd68504d74
Ockey12 <haloupgarou@gmail.com>
2024-05-23 23:39:30 +0900

    updated `ProtocolView` preview

--- RAGESS/RAGESS/Sources/SwiftDiagramView/TypeView/Protocol/ProtocolView.swift
+++ RAGESS/RAGESS/Sources/SwiftDiagramView/TypeView/Protocol/ProtocolView.swift
@@ -1,6 +1,6 @@
 //
 //  ProtocolView.swift
-//  
+//
 //  
 //  Created by Ockey12 on 2024/05/23
 //  
@@ -43,8 +43,126 @@ public struct ProtocolView: View {
         offsetRange: 0 ... 1
     )
 
+    let initializers = [
+        InitializerObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "public init()",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        InitializerObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "public init()",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        InitializerObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "public init()",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        )
+    ]
+    protocolObject.initializers = initializers
+
+    let variableObjects = [
+        VariableObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "public var firstVariable: Int",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        VariableObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "var variable: String { get set }",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        VariableObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "var variable: String { get set }",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        )
+    ]
+    protocolObject.variables = variableObjects
+
+    let functionObjects = [
+        FunctionObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "override func visit(_ node: ProtocolDeclSyntax) -> SyntaxVisitorContinueKind",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        FunctionObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "private func findProperty<T: TypeNestable>(in object: T, matching: (any DeclarationObject) -> Bool) -> PartialKeyPath<T>?",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        FunctionObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "funcion",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        FunctionObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "funcion",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        )
+    ]
+    protocolObject.functions = functionObjects
+
+
+    let parentProtocolObject = ProtocolObject(
+        name: "ParentProtocol",
+        nameOffset: 0,
+        fullPath: "",
+        annotatedDecl: "public protocol ParentProtocol",
+        sourceCode: "",
+        positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+        offsetRange: 0 ... 1
+    )
+
+    let conformDependency = DependencyObject(
+        kind: .protocolInheritance,
+        callerObject: .init(
+            id: protocolObject.id,
+            keyPath: .struct(\.self)
+        ),
+        definitionObject: .init(
+            id: parentProtocolObject.id,
+            keyPath: .protocol(\.self)
+        )
+    )
+
+    protocolObject.objectsThatAreCalledByThisObject.append(conformDependency)
+
     let allDeclarationObjects: [any DeclarationObject] = [
-        protocolObject
+        protocolObject,
+        parentProtocolObject
     ]
 
     return VStack {

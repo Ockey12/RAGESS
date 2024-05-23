aa61ba5e4078cb17a94d02460031fdfbf2bc9f3f
Ockey12 <haloupgarou@gmail.com>
2024-05-23 16:57:59 +0900

    updated `ClassView` preview

--- RAGESS/RAGESS/Sources/SwiftDiagramView/TypeView/Class/ClassView.swift
+++ RAGESS/RAGESS/Sources/SwiftDiagramView/TypeView/Class/ClassView.swift
@@ -43,8 +43,149 @@ public struct ClassView: View {
         offsetRange: 0 ... 1
     )
 
+    let initializers = [
+        InitializerObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "public init()",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        InitializerObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "public init()",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        InitializerObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "public init()",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        )
+    ]
+    subClass.initializers = initializers
+
+    let variableObjects = [
+        VariableObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "public var firstVariable: Int",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        VariableObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "var variable: String { get set }",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        VariableObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "var variable: String { get set }",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        )
+    ]
+    subClass.variables = variableObjects
+
+    let functionObjects = [
+        FunctionObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "override func visit(_ node: ProtocolDeclSyntax) -> SyntaxVisitorContinueKind",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        FunctionObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "private func findProperty<T: TypeNestable>(in object: T, matching: (any DeclarationObject) -> Bool) -> PartialKeyPath<T>?",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        FunctionObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "funcion",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        ),
+        FunctionObject(
+            name: "",
+            nameOffset: 0,
+            fullPath: "",
+            annotatedDecl: "funcion",
+            positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+            offsetRange: 0 ... 1
+        )
+    ]
+    subClass.functions = functionObjects
+
+    let superClass = ClassObject(
+        name: "SuperClass",
+        nameOffset: 0,
+        fullPath: "",
+        annotatedDecl: "public class SuperClass",
+        positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+        offsetRange: 0 ... 1
+    )
+
+    let inheritDependency = DependencyObject(
+        kind: .classInheritance,
+        callerObject: .init(
+            id: subClass.id,
+            keyPath: .class(\.self)
+        ),
+        definitionObject: .init(
+            id: superClass.id,
+            keyPath: .class(\.self)
+        )
+    )
+
+    subClass.objectsThatAreCalledByThisObject.append(inheritDependency)
+
+    let protocolObject = ProtocolObject(
+        name: "ConformedProtocol",
+        nameOffset: 0,
+        fullPath: "",
+        annotatedDecl: "public protocol ConformedProtocol",
+        sourceCode: "",
+        positionRange: SourcePosition(line: 0, utf8index: 0) ... SourcePosition(line: 1, utf8index: 1),
+        offsetRange: 0 ... 1
+    )
+
+    let conformDependency = DependencyObject(
+        kind: .protocolConformance,
+        callerObject: .init(
+            id: subClass.id,
+            keyPath: .struct(\.self)
+        ),
+        definitionObject: .init(
+            id: protocolObject.id,
+            keyPath: .protocol(\.self)
+        )
+    )
+
+    subClass.objectsThatAreCalledByThisObject.append(conformDependency)
+
     let allDeclarationObjects: [any DeclarationObject] = [
-        subClass
+        subClass,
+        superClass,
+        protocolObject
     ]
 
     return VStack {
@@ -60,5 +201,5 @@ public struct ClassView: View {
             )
         )
     }
-    .frame(width: 3500, height: 2000)
+    .frame(width: 3500, height: 2300)
 }

7ca983a12dd869b9656da85184d1b754a3407de0
Ockey12 <haloupgarou@gmail.com>
2024-05-26 08:01:44 +0900

    fixed `ArrowView`

--- RAGESS/RAGESS/Sources/SwiftDiagramView/ArrowView.swift
+++ RAGESS/RAGESS/Sources/SwiftDiagramView/ArrowView.swift
@@ -13,31 +13,29 @@ struct ArrowView: View {
     let endPoint: CGPoint
 
     var body: some View {
-        GeometryReader { _ in
-            let path = Path { path in
-                path.move(to: startPoint)
-                path.addLine(to: endPoint)
+        let path = Path { path in
+            path.move(to: startPoint)
+            path.addLine(to: endPoint)
 
-                // Arrow tip
-                let arrowSize: CGFloat = 10
-                let angle = atan2(endPoint.y - startPoint.y, endPoint.x - startPoint.x)
-                let arrowPoint1 = CGPoint(
-                    x: endPoint.x - arrowSize * cos(angle - CGFloat.pi / 6),
-                    y: endPoint.y - arrowSize * sin(angle - CGFloat.pi / 6)
-                )
-                let arrowPoint2 = CGPoint(
-                    x: endPoint.x - arrowSize * cos(
-                        angle + CGFloat.pi / 6
-                    ),
-                    y: endPoint.y - arrowSize * sin(angle + CGFloat.pi / 6)
-                )
+            // Arrow tip
+            let arrowSize: CGFloat = 10
+            let angle = atan2(endPoint.y - startPoint.y, endPoint.x - startPoint.x)
+            let arrowPoint1 = CGPoint(
+                x: endPoint.x - arrowSize * cos(angle - CGFloat.pi / 6),
+                y: endPoint.y - arrowSize * sin(angle - CGFloat.pi / 6)
+            )
+            let arrowPoint2 = CGPoint(
+                x: endPoint.x - arrowSize * cos(
+                    angle + CGFloat.pi / 6
+                ),
+                y: endPoint.y - arrowSize * sin(angle + CGFloat.pi / 6)
+            )
 
-                path.addLine(to: arrowPoint1)
-                path.move(to: endPoint)
-                path.addLine(to: arrowPoint2)
-            }
-
-            path.stroke(Color.black, lineWidth: 4)
+            path.addLine(to: arrowPoint1)
+            path.move(to: endPoint)
+            path.addLine(to: arrowPoint2)
         }
+
+        path.stroke(Color.black, lineWidth: 4)
     }
 }

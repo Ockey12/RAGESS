98ab7d74694013474895866052dc0b3a1f19720d
Ockey12 <haloupgarou@gmail.com>
2024-05-25 21:02:45 +0900

    applied `topLeadingPoint` to `DetailReducer.State` in `ProtocolViewReducer`

--- RAGESS/RAGESS/Sources/SwiftDiagramView/TypeView/Protocol/ProtocolViewReducer.swift
+++ RAGESS/RAGESS/Sources/SwiftDiagramView/TypeView/Protocol/ProtocolViewReducer.swift
@@ -100,29 +100,78 @@ public struct ProtocolViewReducer {
                 bodyWidth: bodyWidth
             )
 
+
+            let borderWidth = ComponentSizeValues.borderWidth
+            let connectionHeight = ComponentSizeValues.connectionHeight
+            let itemHeight = ComponentSizeValues.itemHeight
+            let bottomPaddingForLastText = ComponentSizeValues.bottomPaddingForLastText
+
+            var frameBottomLeadingPoint = CGPoint(
+                x: topLeadingPoint.x,
+                y: topLeadingPoint.y
+                + borderWidth / 2
+                + itemHeight*2
+                + bottomPaddingForLastText
+            )
+
+            let protocolsFrameTopLeadingPoint = frameBottomLeadingPoint
+            if !parentProtocolObjects.isEmpty {
+                frameBottomLeadingPoint = CGPoint(
+                    x: frameBottomLeadingPoint.x,
+                    y: frameBottomLeadingPoint.y
+                    + connectionHeight
+                    + itemHeight*CGFloat(parentProtocolObjects.count)
+                    + bottomPaddingForLastText
+                )
+            }
+
+            let initializersTopLeadingPoint = frameBottomLeadingPoint
+            if !object.initializers.isEmpty {
+                frameBottomLeadingPoint = CGPoint(
+                    x: frameBottomLeadingPoint.x,
+                    y: frameBottomLeadingPoint.y
+                    + connectionHeight
+                    + itemHeight*CGFloat(object.initializers.count)
+                    + bottomPaddingForLastText
+                )
+            }
+
+            let variablesTopLeadingPoint = frameBottomLeadingPoint
+            if !object.variables.isEmpty {
+                frameBottomLeadingPoint = CGPoint(
+                    x: frameBottomLeadingPoint.x,
+                    y: frameBottomLeadingPoint.y
+                    + connectionHeight
+                    + itemHeight*CGFloat(object.variables.count)
+                    + bottomPaddingForLastText
+                )
+            }
+
+            let functionsTopLeadingPoint = frameBottomLeadingPoint
+
             details = [
                 DetailReducer.State(
                     objects: parentProtocolObjects,
                     kind: .parentProtocol,
-                    topLeadingPoint: topLeadingPoint,
+                    topLeadingPoint: protocolsFrameTopLeadingPoint,
                     frameWidth: bodyWidth
                 ),
                 DetailReducer.State(
                     objects: object.initializers,
                     kind: .initializers,
-                    topLeadingPoint: topLeadingPoint,
+                    topLeadingPoint: initializersTopLeadingPoint,
                     frameWidth: bodyWidth
                 ),
                 DetailReducer.State(
                     objects: object.variables,
                     kind: .variables,
-                    topLeadingPoint: topLeadingPoint,
+                    topLeadingPoint: variablesTopLeadingPoint,
                     frameWidth: bodyWidth
                 ),
                 DetailReducer.State(
                     objects: object.functions,
                     kind: .functions,
-                    topLeadingPoint: topLeadingPoint,
+                    topLeadingPoint: functionsTopLeadingPoint,
                     frameWidth: bodyWidth
                 )
             ]

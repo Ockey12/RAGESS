738bf5832b60aae7e971e6ef15fbd48b8f29c642
Ockey12 <haloupgarou@gmail.com>
2024-07-13 15:46:01 +0900

    added DirectoryCellViewReducer

---
+++ RAGESS/RAGESS/Sources/FileTreeView/DirectoryCellViewReducer.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//  
+//  Created by Ockey12 on 2024/07/13
+//  
+//
+
+import ComposableArchitecture
+import XcodeObject
+
+@Reducer
+struct DirectoryCellViewReducer {
+    @ObservableState
+    struct State {
+        let directory: Directory
+    }
+}

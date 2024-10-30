31fd82fc40936f93e2d599556a950d6361dceafc
Ockey12 <haloupgarou@gmail.com>
2024-10-31 00:59:32 +0900

    add sample files

---
+++ RAGESS/RAGESS/Sources/Sample/Initializer.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//  
+//  Created by Ockey12 on 2024/10/31
+//  
+//
+
+private struct InitializerSample {
+    init () {
+        let _ = 0
+        func method() {}
+
+        protocol NestedProtocol {}
+        struct NestedStruct {}
+        class NestedClass {}
+        enum NestedEnum {}
+        actor NestedActor {}
+    }
+}
---
+++ RAGESS/RAGESS/Sources/Sample/Variable.swift
@@ -0,0 +1,20 @@
+//
+//  Variable.swift
+//
+//  
+//  Created by Ockey12 on 2024/10/31
+//  
+//
+
+private var VariableSample: Int {
+    let _ = 0
+    func method() {}
+
+    protocol NestedProtocol {}
+    struct NestedStruct {}
+    class NestedClass {}
+    enum NestedEnum {}
+    actor NestedActor {}
+
+    return 0
+}

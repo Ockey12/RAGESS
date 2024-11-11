5d011ca7775ff1166489777fbba4e0ff31f133ec
Ockey12 <haloupgarou@gmail.com>
2024-11-12 01:50:31 +0900

    add SwiftIndexStoreClientDebugger

---
+++ RAGESS/RAGESS/Sources/DebugView/SwiftIndexStoreClientDebugger.swift
@@ -0,0 +1,91 @@
+//
+//  File.swift
+//  
+//  
+//  Created by Ockey12 on 2024/11/12
+//  
+//
+
+import ComposableArchitecture
+import Foundation
+import SwiftIndexStoreClient
+import SwiftUI
+
+@Reducer
+public struct SwiftIndexStoreClientDebugger {
+    @ObservableState
+    public struct State {
+        var path: String
+
+        public init(
+            path: String = "/Users/onaga/Library/Developer/Xcode/DerivedData/RAGESS-ayjrlzfdtsotsbgxonebesbohntz/Index.noindex/DataStore"
+        ) {
+            self.path = path
+        }
+    }
+
+    public enum Action: BindableAction {
+        case executeButtonTapped
+        case binding(BindingAction<State>)
+    }
+
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .executeButtonTapped:
+                guard let url = URL(string: state.path) else {
+                    assertionFailure()
+                    return .none
+                }
+                @Dependency(SwiftIndexStoreClient.self) var client
+                do {
+                    let startTime = CFAbsoluteTimeGetCurrent()
+                    let definitions = try client.extractDefinitions(url)
+                    let endTime = CFAbsoluteTimeGetCurrent()
+                    print("COMPLETE SwiftIndexStoreClient.extractDefinitions: \(endTime - startTime)S: \(definitions.count) definitions")
+//                    for definition in definitions {
+//                        print("| user = \(definition.usr) | location = \(definition.fullPath):\(definition.line):\(definition.column)")
+//                    }
+                } catch {
+                    print(error)
+                    assertionFailure()
+                }
+                return .none
+
+            case .binding:
+                return .none
+            }
+        }
+    }
+}
+
+public struct SwiftIndexStoreClientDebugView: View {
+    @Bindable var store: StoreOf<SwiftIndexStoreClientDebugger>
+
+    public init(store: StoreOf<SwiftIndexStoreClientDebugger>) {
+        self.store = store
+    }
+
+    public var body: some View {
+        VStack {
+            HStack {
+                Button(
+                    action: {
+                        store.send(.executeButtonTapped)
+                    },
+                    label: {
+                        Image(systemName: "play.fill")
+                            .font(.system(size: 10))
+                    }
+                )
+                Text("SwiftIndexStoreClient.extractDefinitions()")
+                    .font(.system(size: 13))
+                Spacer()
+            }
+
+            TextField("Index Store path", text: $store.path)
+                .padding(.horizontal)
+        }
+        .padding()
+    }
+}

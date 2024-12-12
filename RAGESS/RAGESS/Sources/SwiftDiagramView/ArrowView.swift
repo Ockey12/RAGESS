159593fc47d1e04d0c807a7e54bac232da354128
Ockey12 <haloupgarou@gmail.com>
2024-12-12 19:13:06 +0900

    update: arrow tips

--- RAGESS/RAGESS/Sources/SwiftDiagramView/ArrowView.swift
+++ RAGESS/RAGESS/Sources/SwiftDiagramView/ArrowView.swift
@@ -20,24 +20,49 @@ struct ArrowView: View {
             path.addLine(to: store.endPoint)
 
             // Arrow tip
-            let arrowSize: CGFloat = 30
-            let angle = atan2(store.endPoint.y - store.startPoint.y, store.endPoint.x - store.startPoint.x)
-            let arrowPoint1 = CGPoint(
-                x: store.endPoint.x - arrowSize * cos(angle - CGFloat.pi / 6),
-                y: store.endPoint.y - arrowSize * sin(angle - CGFloat.pi / 6)
-            )
-            let arrowPoint2 = CGPoint(
-                x: store.endPoint.x - arrowSize * cos(
-                    angle + CGFloat.pi / 6
-                ),
-                y: store.endPoint.y - arrowSize * sin(angle + CGFloat.pi / 6)
+            let arrowSize: CGFloat = 15
+
+            // first
+            let firstTipPoint = CGPoint(
+                x: store.startPoint.x + (store.endPoint.x - store.startPoint.x) / 3,
+                y: store.startPoint.y + (store.endPoint.y - store.startPoint.y) / 3
             )
 
-            path.addLine(to: arrowPoint1)
-            path.move(to: store.endPoint)
-            path.addLine(to: arrowPoint2)
+            let firstAngle = atan2(firstTipPoint.y - store.startPoint.y, firstTipPoint.x - store.startPoint.x)
+            let firstTipPoint1 = CGPoint(
+                x: firstTipPoint.x - arrowSize * cos(firstAngle - CGFloat.pi / 6),
+                y: firstTipPoint.y - arrowSize * sin(firstAngle - CGFloat.pi / 6)
+            )
+            let firstTipPoint2 = CGPoint(
+                x: firstTipPoint.x - arrowSize * cos(firstAngle + CGFloat.pi / 6),
+                y: firstTipPoint.y - arrowSize * sin(firstAngle + CGFloat.pi / 6)
+            )
+            path.move(to: firstTipPoint)
+            path.addLine(to: firstTipPoint1)
+            path.move(to: firstTipPoint)
+            path.addLine(to: firstTipPoint2)
+
+            // second
+            let secondTipPoint = CGPoint(
+                x: store.startPoint.x + (store.endPoint.x - store.startPoint.x) * 2 / 3,
+                y: store.startPoint.y + (store.endPoint.y - store.startPoint.y) * 2 / 3
+            )
+
+            let secondAngle = atan2(secondTipPoint.y - store.startPoint.y, secondTipPoint.x - store.startPoint.x)
+            let secondTipPoint1 = CGPoint(
+                x: secondTipPoint.x - arrowSize * cos(secondAngle - CGFloat.pi / 6),
+                y: secondTipPoint.y - arrowSize * sin(secondAngle - CGFloat.pi / 6)
+            )
+            let secondTipPoint2 = CGPoint(
+                x: secondTipPoint.x - arrowSize * cos(secondAngle + CGFloat.pi / 6),
+                y: secondTipPoint.y - arrowSize * sin(secondAngle + CGFloat.pi / 6)
+            )
+            path.move(to: secondTipPoint)
+            path.addLine(to: secondTipPoint1)
+            path.move(to: secondTipPoint)
+            path.addLine(to: secondTipPoint2)
         }
 
-        path.stroke(Color("Arrow", bundle: .module), lineWidth: 10)
+        path.stroke(Color("Arrow", bundle: .module), lineWidth: 3)
     }
 }
